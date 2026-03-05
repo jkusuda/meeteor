354ffbb0d517aa020dcbecbb68270a220beb0697
Joyce-Maniquis <maniquisjoyce@ufl.edu>
2026-03-05 16:48:11 -0500

    Added website color palette to main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,14 @@
 import 'package:astrophotography_blog/screens/home.dart';
 import 'package:flutter/material.dart';
 
+class AppColors {
+  static const Color prussianBlue = Color(0xFF141C34);
+  static const Color spaceIndigo = Color(0xFF302C5C);
+  static const Color vintageLavender = Color(0xFF73628A);
+  static const Color thistle = Color(0xFFCBC3D5);
+  static const Color honeyBronze = Color(0xFFFCB454);
+}
+
 void main() {
   runApp(const MyApp());
 }
@@ -11,9 +19,22 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage()
+      theme: ThemeData(
+        colorScheme: const ColorScheme.light(
+          primary: AppColors.spaceIndigo,
+          secondary: AppColors.honeyBronze,
+          tertiary: AppColors.vintageLavender,
+          surface: AppColors.thistle,
+          onPrimary: Colors.white,
+          onSecondary: AppColors.prussianBlue,
+          onTertiary: Colors.white,
+          onSurface: AppColors.prussianBlue,
+        ),
+        scaffoldBackgroundColor: AppColors.thistle,
+      ),
+      home: const HomePage(),
     );
   }
 }
