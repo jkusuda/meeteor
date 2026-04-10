338b58498bbf34f4c10d1857fc7b4340c360dccb
sydney <sydneylee1510@gmail.com>
2026-04-10 13:24:03 -0400

    fixes to profile grid

--- lib/widgets/profile_post_card.dart
+++ lib/widgets/profile_post_card.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:meeteor/widgets/post_card.dart';
+import 'package:go_router/go_router.dart';
 
 class ProfilePostCard extends StatelessWidget {
   final Map<String, dynamic> post;
@@ -8,9 +8,30 @@ class ProfilePostCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ClipRRect(
-      borderRadius: BorderRadius.circular(12),
-      child: PostCard(post: post),
+    final imageUrl = post['imageUrl'] ?? post['image_url'];
+
+    return GestureDetector(
+      onTap: () {
+        context.push('/p/${post['id']}');
+      },
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(12),
+        child: imageUrl != null
+            ? Image.network(
+                imageUrl,
+                fit: BoxFit.cover,
+                width: double.infinity,
+                height: double.infinity,
+              )
+            : Container(
+                color: Colors.black45,
+                child: const Icon(
+                  Icons.image_not_supported,
+                  color: Colors.white54,
+                  size: 40,
+                ),
+              ),
+      ),
     );
   }
 }
